940e2344825f52be9192993af4f5068870889f30
Vince Ian Escopete <vinceianescopete07@gmail.com>
2025-06-11 14:17:53 +0800

    feat: add Intro component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
+import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+      <Intro />
       <SectionDivider />
     </main>
   );
